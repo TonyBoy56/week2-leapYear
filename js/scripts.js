c79580d4f9af2e149f7fd00651921e5bcb45c53d
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-01 11:21:03 -0700

    add new spec. Work on error handling for number inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
     var input = $("input#userInput").val();
+    var inputArray = input.split("");
     var vowelsArray = ["a", "e", "i" , "o", "u", "A", "E", "I", "O", "U"];
-    input.split("");
-    console.log(input);
+    console.log(inputArray);
     if (input === "") {
       alert("Invalid submission! Please enter a phrase!");
-    //} else if (input === "i", "a") {
-      //$("#output").text(input + "ay");
+    } else if (input === int || float) {
+      $("#output").text(input);
+    } else if (inputArray.includes(vowelsArray)) {
+      inputArray.join();
+      $("#output").text(inputArray + "ay");
     }
   });
 });
