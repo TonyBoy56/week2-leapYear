84e01084eb2899a609154d9b80a04c5d049a36cb
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-01 14:18:29 -0700

    add functionality for 'way' at the end of wordes starting with a vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,30 @@ $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#userInput").val();
-    var vowelsArray = ["a", "e", "i" , "o", "u", "A", "E", "I", "O", "U"];
+    var numberInput = parseInt(userInput);
+    console.log(userInput);
+    var vowelsArray = ["a", "e", "i" , "o", "u"];
     var consonatesArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z"];
-    var inputArray = userInput.split("");
     
+    if (numberInput === true) {
+      $("#output").text("'"+ userInput+ "'" + " is a number, my dude. How about some words next time?")
+    }
     for (var i = 0; i < vowelsArray.length; i++) {
-      if (userInput.includes(i)) {
-        $("#output").text(userInput);
+      if (vowelsArray[i] === userInput.includes("i") || vowelsArray[i] === userInput.includes("a")){
+        $("#output").text(userInput + "ay");
+      } else if (vowelsArray[i] === userInput.charAt(0)) {
+       $("#output").text(userInput + "way");
+      }
+    }
+    for (var i = 0; i < consonatesArray.length; i++) {
+      if (consonatesArray[i] === userInput.charAt(0)) {
+       $("#output").text(userInput + "way");
       }
     }
   });
 });
 
+
+  // else {
+  //  $("#output").text(phrase.slice(1, phrase.length) + phrase.charAt(0) + "ay");
+  // }
