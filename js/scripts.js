fa1c026658acfacb154e10e45bc93aa650e6010f
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-01 15:34:02 -0700

    include branding statement for inputs beginning with 'qu'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
+// Business Interface
+function isVowel(letter) {
+  var vowelsArray = ["a", "e", "i" , "o", "u"];
+
+  for (var i = 0; i < vowelsArray.length; i++) {
+    if (vowelsArray[i] === letter) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function igPayatinLay(userInput) {
+  var numberInput = parseInt(userInput);
+    if (numberInput) {
+      return"'"+ userInput+ "'" + " is a number, my dude. How about some words next time?";
+    } else if (userInput === "i" || userInput === "a"){
+      return userInput + "ay";
+    } else if (isVowel(userInput.charAt(0)) ) {
+      return userInput + "way";
+    } else if (userInput.slice(0, 2) === "qu") {
+      return  userInput.slice(2) + userInput.slice(0,2) + "ay";
+    } else {
+     for (var i = 0; i < userInput.length; i++) {
+      if ( isVowel(userInput.charAt(i))) {
+        return userInput.slice(i)       //sliced from the vowel to the end
+         + userInput.slice(0,i) //from beginning of word to the vowel but not including the vowel
+         + "ay";    // then adds "AY"
+      }
+    }
+  }
+}
+
+// UI
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#userInput").val();
-    var numberInput = parseInt(userInput);
-    console.log(userInput);
-    var vowelsArray = ["a", "e", "i" , "o", "u"];
-    var consonatesArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z"];
-    
-    if (numberInput === true) {
-      $("#output").text("'"+ userInput+ "'" + " is a number, my dude. How about some words next time?")
-    }
-    for (var i = 0; i < vowelsArray.length; i++) {
-      if (userInput === "i" || userInput === "a"){
-        $("#output").text(userInput + "ay");
-      } else if (vowelsArray[i] === userInput.charAt(0)) {
-       $("#output").text(userInput + "way");
-      }
-    }
-    for (var i = 0; i < consonatesArray.length; i++) {
-      if (consonatesArray[i] === userInput.charAt(0)) {
-       $("#output").text(userInput + "way");
-      }
-    }
+    $('#output').text(igPayatinLay(userInput));
   });
 });
